42b15f823d083147d125d2b54f10520d684a634a
Daniel Spilker <mail@daniel-spilker.com>
2009-04-07 20:07:33 +0200

    missing

---
+++ Util/AbortableTaskExecutor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DasBackupTool.Util
+{
+    public class AbortableTaskExecutor : IDisposable
+    {
+        private object monitor = new object();
+        private Thread thread;
+        private ThreadStart task;
+
+        public AbortableTaskExecutor(ThreadStart task)
+        {
+            this.task = task;
+        }
+
+        public void Run()
+        {
+            lock (monitor)
+            {
+                if (thread != null)
+                {
+                    Abort();
+                }
+                thread = new Thread(task);
+                thread.Start();
+            }
+        }
+
+        public void Dispose()
+        {
+            Abort();
+        }
+
+        public void Abort()
+        {
+            lock (monitor)
+            {
+                if (thread != null)
+                {
+                    thread.Abort();
+                    thread.Join();
+                    thread = null;
+                }
+            }
+        }
+    }
+}
