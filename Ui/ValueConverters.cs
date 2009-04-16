95555b26c60e04380ae19c6c689249ff62f1fec1
Daniel Spilker <mail@daniel-spilker.com>
2009-04-16 18:49:38 +0200

    file icons

--- Ui/ValueConverters.cs
+++ Ui/ValueConverters.cs
@@ -9,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using DasBackupTool.Util;
 using System.Text;
+using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace DasBackupTool.Ui
 {
@@ -69,12 +71,64 @@ namespace DasBackupTool.Ui
         static FileIconValueConverter()
         {
             directoryImageSource = ExtractIcon("shell32.dll", 3);
-            fileImageSource = ExtractIcon("shell32.dll", 1);
+            fileImageSource = ExtractIcon("shell32.dll", 0);
         }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null ? null : FileUtils.IsDirectory((string)value) ? directoryImageSource : fileImageSource;
+            if (value == null) return null;
+            ImageSource result = null;
+            if (FileUtils.IsDirectory((string)value))
+            {
+                result = directoryImageSource;
+            }
+            else
+            {
+                DateTime start = DateTime.Now;
+                string extension = new FileInfo((string)value).Extension;
+                RegistryKey key = Registry.ClassesRoot.OpenSubKey(extension);
+                if (key != null)
+                {
+                    string fileType = (string)key.GetValue(null);
+                    if (fileType != null)
+                    {
+                        key = Registry.ClassesRoot.OpenSubKey(fileType + "\\DefaultIcon");
+                        if (key != null)
+                        {
+                            string libraryAndIndex = (string)key.GetValue(null);
+                            if (libraryAndIndex != null)
+                            {
+                                string library;
+                                int index;
+                                if (libraryAndIndex == "%1" || libraryAndIndex == "\"%1\"")
+                                {
+                                    library = (string)value;
+                                    index = 0;
+                                }
+                                else if (libraryAndIndex.ToLower().EndsWith(".ico"))
+                                {
+                                    library = libraryAndIndex;
+                                    index = 0;
+                                }
+                                else
+                                {
+                                    int commaPos = libraryAndIndex.LastIndexOf(",");
+                                    library = libraryAndIndex.Substring(0, commaPos);
+                                    index = int.Parse(libraryAndIndex.Substring(commaPos + 1));
+                                }
+                                DateTime end = DateTime.Now;
+                                Debug.Print("registry lookup took " + (end - start));
+                                result = ExtractIcon(library, index);
+                            }
+                        }
+                    }
+                }
+            }
+            if (result == null)
+            {
+                result = fileImageSource;
+            }
+            return result;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -88,14 +142,15 @@ namespace DasBackupTool.Ui
         [DllImport("User32.dll")]
         private extern static int DestroyIcon(IntPtr hIcon);
 
-        private static ImageSource ExtractIcon(string libraryName, int index)
+        private static BitmapSource ExtractIcon(string libraryName, int index)
         {
+            DateTime start = DateTime.Now;
             IntPtr[] smallIcon = new IntPtr[1];
-            ExtractIconEx(libraryName, index, null, smallIcon, 1);
+            int result = ExtractIconEx(libraryName, index, null, smallIcon, 1);
+            if (result == 0) return null;
             try
             {
-                Icon icon = Icon.FromHandle(smallIcon[0]);
-                using (icon)
+                using (Icon icon = Icon.FromHandle(smallIcon[0]))
                 {
                     Bitmap bitmap = icon.ToBitmap();
                     MemoryStream memoryStream = new MemoryStream();
@@ -111,6 +166,8 @@ namespace DasBackupTool.Ui
             finally
             {
                 DestroyIcon(smallIcon[0]);
+                DateTime end = DateTime.Now;
+                Debug.Print("extract icon took " + (end - start));
             }
         }
     }
